33a1705e03b6ca06e93b2498ec0f8a715416cf7f
Ditto P S <resources@accubits.com>
2023-04-17 02:03:05 +0530

    breadcrub ui

--- renderer/components/layouts/side-menu-layout/index.tsx
+++ renderer/components/layouts/side-menu-layout/index.tsx
@@ -43,7 +43,7 @@ export default function SideMenuLayout({ children, showTopBar }) {
 
   const { push } = useRouter();
   const newPage = async () => {
-    //const pathDoc = await ipcRenderer.invoke("app-get-path", "documents");
+    const pathDoc = await ipcRenderer.invoke("app-get-path", "documents");
     console.log("Path", pathDoc);
     const file = await createNewFile(pathDoc);
     console.log("File", file);
@@ -96,102 +96,80 @@ export default function SideMenuLayout({ children, showTopBar }) {
   };
 
   return (
-    <div
-      className={
-        collapsed
-          ? `${styles.layoutWrapper} ${styles.collapsed}`
-          : `${styles.layoutWrapper}`
-      }
-    >
-      <div className={styles.sidePanelWrapper}>
+    <div className={collapsed ? `${styles.layoutWrapper} ${styles.collapsed}` : `${styles.layoutWrapper}`}>
+    <div className={styles.sidePanelWrapper}>
         <div className={styles.topWrap}>
-          <div
-            className={styles.logo}
-            style={{ backgroundImage: "url(/images/logo.png)" }}
-          ></div>
-          <div
-            className={styles.collapseWrap}
-            onClick={() => setCollapsed(!collapsed)}
-          >
-            <ChevronDoubleLeftIcon className="icons" />
-          </div>
+            <div className={styles.logo} style={{backgroundImage: 'url(/images/logo.png)'}}></div>
+            <div className={styles.collapseWrap} onClick={() => setCollapsed(!collapsed)}>
+                <ChevronDoubleLeftIcon className="icons" />
+            </div>
         </div>
         <div className={styles.menuWrap}>
-          <div className={styles.menuItem}>
-            <MagnifyingGlassIcon className={styles.icons} />
-            <p>Search</p>
-            <div className={styles.menuInfo}>⌘ F</div>
-          </div>
-          <div className={styles.menuItem}>
-            <StarIcon className={styles.icons} />
-            <p>Favourites</p>
-            <div className={styles.menuInfo}>⌘ U</div>
-          </div>
-          <Link className={styles.menuItem} href="/canvas">
-            <BellIcon className={styles.icons} />
-            <p>Notification</p>
-            <div className={styles.menuInfo}>⌘ N</div>
-          </Link>
-          <Link className={`${styles.menuItem} ${styles.active}`} href="/home">
-            <QueueListIcon className={styles.icons} />
-            <p>My Spaces</p>
-            <div className={styles.menuInfo}>⌘ S</div>
-          </Link>
+            <div className={styles.menuItem}>
+                <MagnifyingGlassIcon className={styles.icons} />
+                <p>Search</p>
+                <div className={styles.menuInfo}>⌘ F</div>
+            </div>
+            <div className={styles.menuItem}>
+                <StarIcon className={styles.icons} />
+                <p>Favourites</p>
+                <div className={styles.menuInfo}>⌘ U</div>
+            </div>
+            <Link className={styles.menuItem} href="/canvas">
+                <BellIcon className={styles.icons} />
+                <p>Notification</p>
+                <div className={styles.menuInfo}>⌘ N</div>
+            </Link>
+            <Link className={`${styles.menuItem} ${styles.active}`} href='/home'>
+                <QueueListIcon className={styles.icons} />
+                <p>My Spaces</p>
+                <div className={styles.menuInfo}>⌘ S</div>
+            </Link>
 
-          {/* <Link className={`${styles.menuItem}`} href="/projects/new/new?mode=new">
-                        <QueueListIcon className={styles.icons} />
-                        <p>+ New</p>
-                        <div className={styles.menuInfo}>⌘ D</div>
-                    </Link> */}
+            {/* <Link className={`${styles.menuItem}`} href="/projects/new/new?mode=new">
+                <QueueListIcon className={styles.icons} />
+                <p>+ New</p>
+                <div className={styles.menuInfo}>⌘ D</div>
+            </Link> */}
 
-          {/* <Link href={{ pathname: '/projects/test/'+Math.random().toString(), query: { mode: 'new' } }}>path</Link> */}
 
-          <div className={styles.menuItem} onClick={newPage}>
-            New
-          </div>
+            {/* <Link href={{ pathname: '/projects/test/'+Math.random().toString(), query: { mode: 'new' } }}>path</Link> */}
+
+            <div className={styles.menuItem} onClick={newPage}>
+                New
+            </div>
+
+
+
+            
+            
         </div>
         <div className={styles.workspacesWrap}>
-          <div className={styles.workspaceTitle}>
-            <p>Work Spaces</p>
-            <div
-              className={styles.addIcon}
-              onClick={() => setShowAddWorkspace(!showAddWorkspace)}
-            >
-              +
+            
+            <div className={styles.workspaceTitle}>
+                <p>Work Spaces</p>
+                <div className={styles.addIcon} onClick={() => setShowAddWorkspace(!showAddWorkspace)}>+</div>
             </div>
-          </div>
-          <div className={styles.workspaceListWrap}>
-            {workspaces &&
-              workspaces.map((item: any, index: number) => (
-                <div
-                  className={styles.workspaceItem}
-                  key={index}
-                  onClick={() => setShowWokspaceViewer(!showWorkspaceViewer)}
-                >
-                  <div className={styles.colorTag}></div>
-                  <p style={{ textTransform: "capitalize" }}>{item.name}</p>
-                </div>
-              ))}
-            {showAddWorkspace && (
-              <div className={styles.workspaceItem}>
-                <div className={styles.colorTag}></div>
-                <input
-                  type="text"
-                  placeholder="Space name"
-                  ref={addWorkspaceInput}
-                  onKeyUp={(event) => addWorkspace(event)}
-                />
-              </div>
-            )}
-          </div>
-          {showWorkspaceViewer && <WorkspaceViewer></WorkspaceViewer>}
+            <div className={styles.workspaceListWrap}>
+                {workspaces.map((item: any, index: number) => (
+                    <div className={styles.workspaceItem} key={index} onClick={() => setShowWokspaceViewer(!showWorkspaceViewer)}>
+                        <div className={styles.colorTag}></div>
+                        <p>{item.name}</p>
+                    </div>
+                ))}
+                {showAddWorkspace && <div className={styles.workspaceItem}>
+                    <div className={styles.colorTag}></div>
+                    <input type="text" placeholder='Space name' ref={addWorkspaceInput} onKeyUp={(event) => addWorkspace(event)}/>
+                </div>}
+            </div>
+            {showWorkspaceViewer && <WorkspaceViewer></WorkspaceViewer>}
         </div>
-      </div>
-      <div className={styles.contentWrapper}>
-        {showTopBar && <TopBar></TopBar>}
-        {children}
-        <AskBud></AskBud>
-      </div>
     </div>
+    <div className={styles.contentWrapper} >
+        {showTopBar && <TopBar></TopBar> }
+        <div className={styles.container}>{children}</div>
+        <AskBud></AskBud>
+    </div>
+</div>
   );
 }
--- renderer/components/top-bar/index.tsx
+++ renderer/components/top-bar/index.tsx
@@ -1,11 +1,22 @@
 import Breadcrumbs from "components/breadcrumbs";
 import React from 'react'
 import styles from './topbar.module.css';
+import {  StarIcon, EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
+
 
 export default function TopBar(){
     return(
         <div className={styles.topBarWrap}>
             <Breadcrumbs></Breadcrumbs>
+            <div className={styles.optionsWrap}>
+                <div className={styles.favicon}>
+                    <StarIcon className="icons" />
+                </div>
+                <div className={styles.share}>Share</div>
+                <div className={styles.moreOption}>
+                    <EllipsisHorizontalIcon className="icons" />
+                </div>
+            </div>
         </div>
     )
 }
