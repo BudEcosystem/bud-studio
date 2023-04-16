8925ebc7b4ab0e4ee1ef38e1bbd7fb504224eb74
Ditto P S <resources@accubits.com>
2023-04-16 15:53:46 +0530

    canvas rectable update

--- renderer/components/canvas/index.tsx
+++ renderer/components/canvas/index.tsx
@@ -6,8 +6,8 @@ import styles from './canvas.module.css'
 import RectangleNode from './rectangle'
 
 const initialNodes = [
-    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+    { id: '1', type: 'rectangle', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', type: 'rectangle', position: { x: 0, y: 100 }, data: { label: '2' } },
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
@@ -54,7 +54,7 @@ export default function Canvas(){
             id: getId(),
             type,
             position,
-            data: { label: `${type} node` },
+            data: { label: '' },
         };
 
         setNodes((nds) => nds.concat(newNode));
--- renderer/components/canvas/rectangle/index.tsx
+++ renderer/components/canvas/rectangle/index.tsx
@@ -1,15 +1,22 @@
 
 
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Handle, Position } from 'reactflow';
 import styles from './rectangle.module.css'
 
 
 export default function RectangleNode({ data }) {
 
+    const [value, setValue] = useState(data.label ?? '');
+
+    // useEffect(() => {
+    //     console.log(data)
+    // }, [data])
 
   const onChange = useCallback((evt) => {
     console.log(evt.target.value);
+    setValue(evt.target.value);
+    data.label = evt.target.value;
 
   }, []);
 
@@ -17,7 +24,7 @@ export default function RectangleNode({ data }) {
     <div className={styles.nodeWrap}>
       <Handle type="target" position={Position.Left} />
       <Handle type="target" position={Position.Top} />
-      <input id="text" name="text" value={data.label} onChange={onChange} className={styles.nodeText} />
+      <input id="text" name="text" value={value} onChange={onChange} className={styles.nodeText} />
       <Handle type="source" position={Position.Bottom} id="a" />
       <Handle type="source" position={Position.Right} id="b"/>
     </div>
