0c255e551a1cfc48a77d4d421248389fc6c3b176
Rahul V Ramesh <rahulvramesh@hotmail.com>
2023-04-17 11:57:23 +0530

    color added

--- renderer/components/workspace-viewer/index.tsx
+++ renderer/components/workspace-viewer/index.tsx
@@ -1,87 +1,144 @@
+import React, { useEffect, useRef } from "react";
+import styles from "./workspace-viewer.module.css";
+import {
+  MagnifyingGlassIcon,
+  FolderIcon,
+  EllipsisHorizontalIcon,
+  DocumentIcon,
+} from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
+import { generateColor } from "utils/color-generator";
+import { createProjectFolder } from "actions/pages";
 
-import React, { useEffect, useRef } from 'react'
-import styles from './workspace-viewer.module.css';
-import {MagnifyingGlassIcon, FolderIcon, EllipsisHorizontalIcon, DocumentIcon} from '@heroicons/react/24/outline'
+export default function WorkspaceViewer({
+  selectedWokspace,
+  getBasePath,
+  refreshStorage,
+}) {
 
-export default function WorkspaceViewer(){
+  const initialList = [
+    {
+      name: "People Ops",
+      children: [{ name: "Document A" }, { name: "Document B" }],
+    },
+    { name: "HR", children: [{ name: "Document A" }, { name: "Document B" }] },
+    { name: "Dev", children: [] },
+  ];
 
-    const initialList = [
-        {name: 'People Ops', files: [{ name: 'Document A'}, {name: 'Document B'}]},
-        {name: 'HR', files: [{ name: 'Document A'}, {name: 'Document B'}]},
-        {name: 'Dev', files: []}
-    ]
-    
-    const addFolderInput = useRef(null);
+  const addFolderInput = useRef(null);
+  const { push } = useRouter();
 
-    const [showAddFolder, setShowAddFolder] = React.useState(false);
-    const [folders, setFolders] = React.useState(initialList as any);
+  const [showAddFolder, setShowAddFolder] = React.useState(false);
+  const [folders, setFolders] = React.useState(selectedWokspace as any);
 
-    useEffect(() => {
-        if(showAddFolder) addFolderInput.current.focus()
-    }, [showAddFolder])
-    
-    const addFolder = (event) => {
-        if (event.key != 'Enter') return;
-        let space = {
-            name: event.target.value
-        }
-        folders.push(space);
-        setFolders(folders);
-        setShowAddFolder(!showAddFolder);
-    }
+  useEffect(() => {
+    if (showAddFolder) addFolderInput.current.focus();
+  }, [showAddFolder]);
 
+  useEffect(() => {
+    setFolders(selectedWokspace);
+  },[selectedWokspace]);
 
-    return (
-        <div className={styles.viewerWrap}>
-            <div className={styles.viewerTop}>
-                <div className={styles.spaceTitle}>
-                    <div className={styles.colorTag}></div>
-                    <p>Accubits</p>
-                    <div className={styles.addIcon} onClick={() => setShowAddFolder(true)}>+</div>
-                </div>
-                <div className={styles.moveOption}></div>
-            </div>
-            <div className={styles.searchWrap}>
-                <MagnifyingGlassIcon className={styles.icons} />
-                <input type="text" placeholder='Search' />
-            </div>
-            <div className={styles.listWrap}>
-                {showAddFolder && <div className={`${styles.folder}`}>
-                    <div className={styles.folderTitle}>
-                        <div className={styles.openIcon}></div>
-                        <div className={styles.folderIcon}>
-                            <FolderIcon className='icons' />
-                        </div>
-                        <input type="text" placeholder='Folder name' ref={addFolderInput} onKeyUp={(event) => addFolder(event)}/>
-                    </div>
-                </div>}
-                {folders.map((item: any, index: number) => (
-                    <div className={`${styles.folder} ${styles.open}`}>
-                        <div className={styles.folderTitle}>
-                            <div className={styles.openIcon}></div>
-                            <div className={styles.folderIcon}>
-                                <FolderIcon className='icons' />
-                            </div>
-                            <p>{item.name}</p>
-                            <div className={styles.addIcon}>+</div>
-                            <div className={styles.actionIcon}>
-                                <EllipsisHorizontalIcon className='icons' />
-                            </div>
-                        </div>
-                        {item.files && <div className={styles.folderList}>
-                            {item.files.map((file: any, index: number) => (
-                                <div className={styles.fileItem}>
-                                    <DocumentIcon className='icons' />
-                                    <p>{file.name}</p>
-                                </div>
-                            ))}
-                        </div>}
-                        
-                    </div>
-                ))}
-                
-                
-            </div>
+  const addFolder = (event) => {
+    if (event.key != "Enter") return;
+    // let space = {
+    //   name: event.target.value,
+    // };
+    // folders.push(space);
+    // setFolders(folders);
+
+    // Create New Directory
+    const createFolder = async () => {
+      const response = await createProjectFolder(
+        getBasePath,
+        event.target.value,
+        selectedWokspace.name
+      );
+      console.log(response);
+      refreshStorage();
+    };
+
+    createFolder();
+    setShowAddFolder(!showAddFolder);
+  };
+
+  const goToEditor = (filePath) => {
+    push(`/projects/view?page=bud&filePath=${filePath}`);
+  };
+
+  return (
+    <div className={styles.viewerWrap}>
+      <div className={styles.viewerTop}>
+        <div className={styles.spaceTitle}>
+          <div
+            className={styles.colorTag}
+            style={{
+              backgroundColor: `${generateColor(folders.name)}`,
+            }}
+          ></div>
+          <p>{folders.name}</p>
+          <div
+            className={styles.addIcon}
+            onClick={() => setShowAddFolder(true)}
+          >
+            +
+          </div>
         </div>
-    )
+        <div className={styles.moveOption}></div>
+      </div>
+      <div className={styles.searchWrap}>
+        <MagnifyingGlassIcon className={styles.icons} />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className={styles.listWrap}>
+        {showAddFolder && (
+          <div className={`${styles.folder}`}>
+            <div className={styles.folderTitle}>
+              <div className={styles.openIcon}></div>
+              <div className={styles.folderIcon}>
+                <FolderIcon className="icons" />
+              </div>
+              <input
+                type="text"
+                placeholder="Folder name"
+                ref={addFolderInput}
+                onKeyUp={(event) => addFolder(event)}
+              />
+            </div>
+          </div>
+        )}
+        {folders.children.map((item: any, index: number) => (
+          <div className={`${styles.folder} ${styles.open}`}>
+            <div className={styles.folderTitle}>
+              <div className={styles.openIcon}></div>
+              <div className={styles.folderIcon}>
+                <FolderIcon className="icons" />
+              </div>
+              <p>{item.name}</p>
+              <div className={styles.addIcon}>+</div>
+              <div className={styles.actionIcon}>
+                <EllipsisHorizontalIcon className="icons" />
+              </div>
+            </div>
+            {item.children && (
+              <div className={styles.folderList}>
+                {item.children.map((file: any, index: number) => (
+                  <div className={styles.fileItem}>
+                    <DocumentIcon className="icons" />
+                    <p
+                      onClick={() => {
+                        goToEditor(file.value);
+                      }}
+                    >
+                      {file.name}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
