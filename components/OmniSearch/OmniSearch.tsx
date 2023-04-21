14666a34fc0f028a64a3646933c3f6ae06d555dc
arun-k6 <arun.k@accubits.com>
2023-04-21 13:58:15 +0530

    searchbox

--- components/OmniSearch/OmniSearch.tsx
+++ components/OmniSearch/OmniSearch.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import TopPanel from './omniComponent/TopPanel';
+import SearchBar from './SearchBar/SearchBar';
 
 const OmniSearch = () => {
   return (
     <div className='mainLauncher'>
+      <SearchBar />
       <TopPanel />
     </div>
   )
