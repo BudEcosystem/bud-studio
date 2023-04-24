0bc36f28ad3cd6c8dc2bd2ebd410525c8238befb
arun-k6 <arun.k@accubits.com>
2023-04-24 13:46:35 +0530

    search

--- pages/menu1.tsx
+++ pages/menu1.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import OmniSearch from "../components/OmniSearch/OmniSearch";
 
 export default function App() {
   return (
     <>
       <div>Menu 1</div>
+      <OmniSearch />
     </>
   );
 }
