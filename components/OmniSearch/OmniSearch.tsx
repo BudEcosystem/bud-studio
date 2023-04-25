27b5d81d7e2aa3f4277925f2111c153f87388e5b
arun-k6 <arun.k@accubits.com>
2023-04-25 17:42:10 +0530

    omnisearch is available everywhere

--- components/OmniSearch/OmniSearch.tsx
+++ components/OmniSearch/OmniSearch.tsx
@@ -1,15 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import Panel from './Panel/Panel'
-
+import { Modal } from 'antd';
 
 const OmniSearch = () => {
   const [themeColor, setThemeColor] = useState("aqua");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'm') {
+      setIsModalOpen((!isModalOpen));
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isModalOpen]);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className='OmniSearch'>
-      <SearchBar themeColor={themeColor} />
-      <Panel/>
-    </div>
+    <Modal className="Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <div className='OmniSearch'>
+        <SearchBar themeColor={themeColor} />
+        <Panel />
+      </div>
+    </Modal>
   )
 };
 
