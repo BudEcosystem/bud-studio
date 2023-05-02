ccc6f7d5d63dc8eab9050f434e9d14d82f279252
Nithi <58935041+imNithi@users.noreply.github.com>
2023-05-03 00:55:58 +0530

    husky added for precommits

---
+++ src/components/testprecommit.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+export const PreCommit = () => {
+
+    const [is, setIs] = useState(false);
+    useEffect(() => {
+        setIs(true);
+    }, []);
+
+    return <div>
+
+    </div>
+}
