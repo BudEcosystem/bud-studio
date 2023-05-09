d1ed7bf1a5f807f2fee192cfc50d9237129dee99
arun-k6 <arun.k@accubits.com>
2023-05-09 11:31:10 +0530

    hamburger redux

--- src/components/Hamburger/HamburgerItems.tsx
+++ src/components/Hamburger/HamburgerItems.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Hamburger.css';
+import { useSelector } from 'react-redux';
+
 
 const HamburgerItems = ({ title, selected, onClick }) => {
+  const {workspace}:any = useSelector(state=>state)
+  let { color } = workspace
   return (
     <div className={``} onClick={onClick}>
-      <div className={selected ? "verticalBar" : ""}></div>
+      <div className={selected ? "verticalBar" : ""} style={{ background: selected ? `linear-gradient(180deg, ${color} 0%, rgba(147, 154, 255, 0) 100%)` : 'none' }}></div>
       <div className='hamItemsContainer'>
-        <div className={`square`} style={{ background: selected ? '#939AFF' : 'none' }}></div>
+        <div className={`square`} style={{ background: selected ? color : 'none' }}></div>
         <p
           className="hamItemsTitle"
           style={{ display: selected ? 'block' : 'none' }}
