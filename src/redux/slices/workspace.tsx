3a5c30c4d561bd35ea2b9641cb70ede78494a702
arun-k6 <arun.k@accubits.com>
2023-05-16 15:45:17 +0530

    rename & duplicate functionality

--- src/redux/slices/workspace.tsx
+++ src/redux/slices/workspace.tsx
@@ -1,23 +1,47 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
 const initialState: any = {
-    props:{},
-    color:'#939AFF'
-  };
+  props: {},
+  color: '#939AFF',
+  workSpaceItems: []
+};
 export const workspaceSlice = createSlice({
-    name:'workspace',
-    initialState,
-    reducers:{
-        changeColor:(state,action:PayloadAction<any>)=>{
-            console.log("dchdjchdjchjdchdjchdj",action.payload)
-            state.color = action.payload;
-        },
-        changeWorkSpacePropereties:(state,action:PayloadAction<any>)=>{
-            state.props = {...state.props,...action.payload}
-        }
-    }
-})
+  name: 'workspace',
+  initialState,
+  reducers: {
+    changeColor: (state, action: PayloadAction<any>) => {
+      console.log('dchdjchdjchjdchdjchdj', action.payload);
+      state.color = action.payload;
+    },
+    createWorkspaces: (state, action: PayloadAction<any>) => {
+        console.log("cd", action.payload)
+        state.workSpaceItems.push(action.payload)
+        console.log("dsf",[...state.workSpaceItems])
+        // const arr = [...state.workSpaceItems]
+        // console.log("Asdf" ,arr, [...state.workSpaceItems])
+        // arr[0].name = "dsfsd"
+        // console.log("h" ,arr[0])
 
-export const {changeColor,changeWorkSpacePropereties} = workspaceSlice.actions;
+    },
+    editWorkspaceItem: (state, action: PayloadAction<any>) => {
+        console.log("cja")
+        console.log("cd", action.payload)
+        const arr = [...state.workSpaceItems]
+        console.log("Asdf" ,arr, [...state.workSpaceItems])
+        arr[action.payload.index].name = action.payload.value
+        console.log("h" ,arr[0])
+    },
+    // duplicateWorkspaceItem: (state, action: PayloadAction<any>) => {
+    //     console.log("cja")
+    //     state.workSpaceItems[action.payload.index] = action.payload.value
+    // },
+    changeWorkSpacePropereties: (state, action: PayloadAction<any>) => {
+      state.props = { ...state.props, ...action.payload };
+    },
+  },
+});
+
+export const { changeColor, createWorkspaces, editWorkspaceItem, duplicateWorkspaceItem, changeWorkSpacePropereties } =
+  workspaceSlice.actions;
 export default workspaceSlice.reducer;
