75cfbb2466ad9533b8c955f7c647e9655a827abc
rakesh-accubits <113332662+rakesh-accubits@users.noreply.github.com>
2023-05-17 21:56:15 +0530

    changes

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -11,5 +11,7 @@ export const store = configureStore({
   },
 });
 
+console.log('workspaceslice', store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
