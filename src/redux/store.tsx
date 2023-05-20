d7188cf265f616e9345d1fca2f25a768ca3a064c
rakesh-accubits <113332662+rakesh-accubits@users.noreply.github.com>
2023-05-20 12:24:58 +0530

    implemented redux-persit

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,14 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './slices/counter';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import workspaceReducer from './slices/workspace';
 import contentReducer from './slices/content';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const reducer = combineReducers({
+  workspace: workspaceReducer,
+  content: contentReducer,
+});
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    workspace: workspaceReducer,
-    content: contentReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable no-console */
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import '../styles/globals.css';
@@ -9,29 +10,37 @@ import '../components/OmniSearch/Panel/Panel.css';
 import '../components/OmniSearch/Panel/Button/Button.css';
 import '../components/OmniSearch/SearchBar/SearchBar.css';
 import '../components/OmniSearch/Panel/PanelOption/PanelOption.css';
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 // redux
 import { Provider } from 'react-redux';
 import { ConfigProvider } from 'antd';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import { store } from '../redux/store';
 
+const persistor = persistStore(store);
 const AppLayout = lazy(() => import('../components/Dashboard/index'));
+
 export default function App() {
   return (
-    <Provider store={store}>
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: 'var(--selected-color)',
-          },
-        }}
-      >
-        <Router>
-          <Routes>
-            <Route path="*" element={<AppLayout />} />
-          </Routes>
-        </Router>
-      </ConfigProvider>
-    </Provider>
+    <Suspense fallback={<></>}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: 'var(--selected-color)',
+              },
+            }}
+          >
+            <Router>
+              <Routes>
+                <Route path="*" element={<AppLayout />} />
+              </Routes>
+            </Router>
+          </ConfigProvider>
+        </PersistGate>
+      </Provider>
+    </Suspense>
   );
 }
