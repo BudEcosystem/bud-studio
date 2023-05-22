54c86ad4c9a7e8a440db6d1e5a9ba5f75aa04c24
rakesh-accubits <113332662+rakesh-accubits@users.noreply.github.com>
2023-05-22 17:06:45 +0530

    changes

--- src/components/KanbanNew/kanbanBoard.tsx
+++ src/components/KanbanNew/kanbanBoard.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-props-no-spreading */
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { styled } from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateColumnPosition } from 'redux/slices/kanban';
-import initialData from './data/initial-data';
+import { createNewColumn, updateColumnPosition } from 'redux/slices/kanban';
 import Column from './components/column';
 
 function Kanban() {
@@ -38,6 +37,76 @@ function Kanban() {
     border-radius: 21px 21px 0px 0px;
     margin-top: 15px;
   `;
+
+  const AddNewColumnSection = styled.div`
+    margin-top: 15px;
+    margin-left: 25px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    height: 20px;
+  `;
+  const AddNewColumnSectionColorPicker = styled.div`
+    width: 15px;
+    height: 15px;
+    background: #171718;
+    border-radius: 5.29412px;
+  `;
+  const AddNewColumnBorderLeft = styled.div`
+    width: 0px;
+    height: 17.64px;
+    border: 1.25336px solid #f9d45d;
+    border-radius: 0.313339px;
+    margin-left: 14px;
+  `;
+
+  const AddNewColumnInput = styled.input`
+    width: 105px;
+    height: 21px;
+    left: 331px;
+    top: 327px;
+    background: rgba(217, 217, 217, 0.05);
+    border-radius: 2px;
+    text-align: left;
+    color: #bbbbbb;
+    outline: none;
+    border: none;
+    margin-left: 5px;
+    &::placeholder,
+    &::-webkit-input-placeholder {
+      font-family: 'Noto Sans';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 154.5%;
+      /* identical to box height, or 22px */
+      text-align: left;
+      color: #bbbbbb;
+    }
+    &:-ms-input-placeholder {
+      font-family: 'Noto Sans';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 154.5%;
+      /* identical to box height, or 22px */
+      color: #bbbbbb;
+    }
+  `;
+  const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  useEffect(() => {
+    const input = document.getElementById(`newColumninput`);
+    input?.addEventListener('keypress', function (event) {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        console.log('Enter clikcked', inputRef.current?.value);
+        if (inputRef.current?.value) {
+          console.log('Enter clikcked', inputRef.current?.value);
+          dispatch(createNewColumn({ name: inputRef.current?.value }));
+        }
+      }
+    });
+  });
   return (
     <ContainerWrapper>
       <ContainerTopBar />
@@ -69,6 +138,15 @@ function Kanban() {
                 );
               })}
               {provided.placeholder}
+              <AddNewColumnSection>
+                <AddNewColumnSectionColorPicker />
+                <AddNewColumnBorderLeft />
+                <AddNewColumnInput
+                  ref={inputRef}
+                  id="newColumninput"
+                  placeholder="Add New"
+                />
+              </AddNewColumnSection>
             </Container>
           )}
         </Droppable>
