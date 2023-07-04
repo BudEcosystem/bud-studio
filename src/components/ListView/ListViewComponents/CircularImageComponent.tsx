a91159e94a398cedf6eb965e1963c79f501e6e32
arun-k6 <arun.k@accubits.com>
2023-07-04 18:21:59 +0530

    removed radix-ui

--- src/components/ListView/ListViewComponents/CircularImageComponent.tsx
+++ src/components/ListView/ListViewComponents/CircularImageComponent.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import * as Avatar from '@radix-ui/react-avatar';
+// import * as Avatar from '@radix-ui/react-avatar';
+import { AntDesignOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Divider, Tooltip } from 'antd';
 
 const CircularImageComponent = ({ images = [] }) => {
   return (
     <div className="circularImageContainer">
-      <Avatar.Root className="AvatarRoot">
+      <Avatar.Group>
+        <Avatar
+          size={24}
+          style={{ backgroundColor: '#f56a00', border: '1px solid black' }}
+          src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+        />
+        <a href="https://ant.design">
+          <Avatar
+            size={24}
+            style={{ backgroundColor: '#f56a00', border: '1px solid black' }}
+          >
+            K
+          </Avatar>
+        </a>
+        <Tooltip title="Ant User" placement="top">
+          <Avatar
+            size={24}
+            style={{ backgroundColor: '#87d068', border: '1px solid black' }}
+            src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
+          />
+        </Tooltip>
+        <Avatar
+          size={24}
+          style={{ backgroundColor: '#1677ff', border: '1px solid black' }}
+          icon={<AntDesignOutlined rev={undefined} />}
+        />
+      </Avatar.Group>
+      {/* <Avatar.Root className="AvatarRoot">
         <Avatar.Image
           className="AvatarImage"
           src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
@@ -31,7 +60,7 @@ const CircularImageComponent = ({ images = [] }) => {
           alt="Colm Tuite"
         />
         <Avatar.Fallback className="AvatarFallback">PD</Avatar.Fallback>
-      </Avatar.Root>
+      </Avatar.Root> */}
       {/* {images.map(item => (<div className='imgCircular'></div>))} */}
       <div className="imgCircular imgCircularTxt">+8</div>
     </div>
