c1c72babeb4b63cf1088945b922089c46ecf7dcb
arun-k6 <arun.k@accubits.com>
2023-07-28 18:23:45 +0530

    table drag

--- src/components/Database/TableView/index.tsx
+++ src/components/Database/TableView/index.tsx
@@ -5,6 +5,7 @@
  */
 
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import {
   DataEditor,
@@ -390,7 +391,102 @@ export default function TableView({
   const onAddRowDrag = (e: DraggableEvent, ui: DraggableData) => {
     // Push The State
     const newData = [...data];
-    newData.push({ name: '' });
+    console.log(newData,"popopo")
+    newData.push( {
+      name: 'Untitled',
+      childOf: null,
+      workSPaceId: 'Private',
+      description: '',
+      type: 'doc',
+      uuid: '39b08a3d-12f1-4651-90f7-328952849dca',
+      workSpaceUUID: '3717e4c0-6b5e-40f2-abfc-bfa4f22fcdcc',
+      customProperties: [
+        {
+          title: 'Author',
+          value: 'Bud',
+          type: 'text',
+          id: '3717e4c0-6b5e-40f2-abfc-bfa4f22gcdcc',
+          order: 4,
+        },
+        {
+          title: 'ISBN',
+          value: 'QWDE-DJJC-1234',
+          type: 'text',
+          id: '3717e4c0-6b5e-40f2-abfc-bfa4f22fcdee',
+          order: 5,
+        },
+      ], // User defined Properties
+      properties: [
+        {
+          title: 'Tags',
+          value: ['no-tag'],
+          type: 'tags',
+          id: '3717e4c0-6b5e-40f2-abfc-bfa4f22gcdc1',
+          order: 1,
+        },
+        {
+          title: 'Priority',
+          value: 'Normal',
+          type: 'priority',
+          id: '3717e4c0-6b5e-40f2-abfc-bfa4f22gcdc2',
+          order: 2,
+        },
+        {
+          title: 'Status',
+          value: 'not_started',
+          type: 'status',
+          id: '3717e4c0-6b5e-40f2-abfc-bfa4f22gcdc3',
+          order: 3,
+        },
+        {
+          title: 'Date',
+          value: null,
+          type: 'date',
+          id: '3717e4c0-6b5e-40f2-abfc-bfa4f22gcdc4',
+          order: 4,
+          startDate: null,
+          endDate: null
+        },
+      ],
+      checkList: [
+        {
+          id: 'abcd',
+          checked: false,
+          title: 'Do homework',
+          createdAt: '',
+          updatedAt: '',
+        },
+        {
+          id: 'efjh',
+          checked: true,
+          title: 'Buy Milk',
+          createdAt: '',
+          updatedAt: '',
+        },
+        {
+          id: 'ijkl',
+          checked: false,
+          title: 'Repair something',
+          createdAt: '',
+          updatedAt: '',
+        },
+        {
+          id: 'mnop',
+          checked: true,
+          title: 'Lol key',
+          createdAt: '',
+          updatedAt: '',
+        },
+      ],
+
+      // System Defined Properties
+      // {39b08a3d-12f1-4651-90f7-328952849xyz
+      //   tags: ['no-tag'],
+      //   priority: 'Normal',
+      //   status: 'Not Started',
+      //   date: null,
+      // },
+    },);
     setData(newData);
   };
 
